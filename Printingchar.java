1daa4a09ea9b2ca9504a72ba94c3adbc6b3789d7
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-06 11:30:42 +0530

    Add files via upload

---
+++ Printingchar.java
@@ -0,0 +1,12 @@
+public class Printingchar{
+    public static void main(String args[]){
+       char c='a';
+       int b=c;
+       b+=1;
+       char v=(char)b;
+       System.out.println(v);
+    }
+}       
+
+
+
