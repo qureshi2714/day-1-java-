626c4e8394b34003d251c0c6da20404c4adf1641
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-06 11:08:48 +0530

    Add files via upload

---
+++ Cast.java
@@ -0,0 +1,9 @@
+public class Cast{
+    public static void main(String args[]){
+        int a=50;
+        char b=(char)(a);
+        char c='7';
+        System.out.println(b);
+        System.out.println(c);
+    }
+}        
