85840f5d4b2cdd5aae856bed4c692d08cc43215c
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-06 11:19:18 +0530

    Add files via upload

---
+++ b.java
@@ -0,0 +1,10 @@
+public class B{
+    public static void main(String args[]){
+        int a=12;
+        char b='a';
+        float c=3.14f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}        
